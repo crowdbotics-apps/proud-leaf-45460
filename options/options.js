0c2601a3c22e08f8574c85bfd729d4749ae7eb2e
Crowdbotics <team@crowdbotics.com>
2024-01-20 23:03:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-leaf-45460",
+  url: "https://proud-leaf-45460.botics.co",
+  api: "https://proud-leaf-45460.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
